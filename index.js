1269bcd7b00974aea8f05a0dd7c8627914de690d
Ayesha Nadeem <162890992+AyeshaNadeem123@users.noreply.github.com>
2024-04-07 18:56:59 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let Convertion = {
+    "PKR": {
+        "USD": 0.0044,
+        "GBP": 0.0037,
+        "PKR": 1
+    },
+    "GBP": {
+        "USD": 1.21,
+        "PKR": 271.79,
+        "GBP": 1
+    },
+    "USD": {
+        "PKR": 225.50,
+        "GBP": 0.83,
+        "USD": 1
+    }
+};
+const answers = await inquirer.prompt([
+    {
+        type: "list",
+        name: "From",
+        choices: ["PKR", "USD", "GBP"],
+        message: "Select your currency:"
+    },
+    {
+        type: "list",
+        name: "to",
+        choices: ["PKR", "USD", "GBP"],
+        message: "Select your convertion currency:"
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "Enter your convertion amount:"
+    }
+]);
+const { from, to, amount } = answers;
+if (from && to && amount) {
+    let result = Convertion[from][to] * amount;
+    console.log(`Your convertion from ${from} to ${to} is ${result}`);
+}
+else {
+    console.log("Invalid inputs");
+}
